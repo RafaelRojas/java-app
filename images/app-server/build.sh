a029289b15c107802a9ee78bfdb4494496dacff8
Rafael Rojas <root@rafaelrojas.net>
2019-08-06 16:54:12 +0000

    Delete build.sh

--- images/app-server/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -x
-
-packer build -var "aws_access_key_id=AKIA5VCYEUTWZ2OPOKL6" -var "aws_secret_access_key=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"   -var "aws_region=us-east-2"  packer.json
-
-
